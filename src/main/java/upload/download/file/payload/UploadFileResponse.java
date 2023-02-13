7c7af5e5bc4ef2d06c1aefe1f6b63c51ba5daa2e
youssefjn <youssefjneyah@gmail.com>
2023-02-13 13:30:17 +0100

    payload & exceptions

---
+++ src/main/java/upload/download/file/payload/UploadFileResponse.java
@@ -0,0 +1,17 @@
+package upload.download.file.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UploadFileResponse {
+    private String fileName;
+    private String fileDownloadUri;
+    private String fileType;
+    private long size;
+
+   
+}
